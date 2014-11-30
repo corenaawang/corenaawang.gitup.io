b90752e9bf3aa952166cace65ac864bfb7a3a8e8
Corena Wang <corenawang@Corenas-MacBook-Pro.local>
2014-11-30 11:58:59 -0800

    my web
    in process

---
+++ FINAL 3/assets/js/script.js
@@ -0,0 +1,15 @@
+jQuery(document).ready(function() {
+  	$('#showmenu').click(function() {
+  		$('.menu').toggle("slide");
+
+   });
+
+});
+
+
+//  $("#p1").click(function(){
+  	// $("#p3").hide();
+  	// $("#p1").click(function(){
+  	// $("#p3").hide();
+
+  
